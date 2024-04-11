5ea89e9c29843ab9c2a38ae7ec3d038443a10b0c
Pooya Parsa <pooya@pi0.io>
2024-04-11 02:15:53 +0200

    try default pages_build_output_dir

--- nitro.config.ts
+++ nitro.config.ts
@@ -8,6 +8,7 @@ export default defineNitroConfig({
   },
   cloudflare: {
     wrangler: {
+      pages_build_output_dir: "dist",
       compatibility_date: "2024-04-10",
       compatibility_flags: ["nodejs_compat"],
     },
