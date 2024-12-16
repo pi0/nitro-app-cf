aaf3574bfdcde4df983c0a6decee26ebd0dec373
Pooya Parsa <pooya@pi0.io>
2024-12-16 22:27:11 +0100

    update

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,16 +3,8 @@ export default defineNitroConfig({
   srcDir: "server",
   minify: false,
   preset: "cloudflare-pages",
+  compatibilityDate: '2024-12-16',
   unenv: {
     external: ["node:async_hooks"],
-  },
-  cloudflare: {
-    wrangler: {
-      // These are to be added by Nitro
-      name: "worker",
-      pages_build_output_dir: "./dist",
-      compatibility_date: "2024-04-10",
-      compatibility_flags: ["nodejs_compat"],
-    },
   },
 });
